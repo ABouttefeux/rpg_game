a014a5fb000810a4420577684cd9cf08c29fdf7b
ABouttefeux <ABouttefeux@users.noreply.github.com>
2022-03-17 19:45:15 +0000

    deploy: efc1381f7ac02cd6f31d5f5ed4935cc32a616aac

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"rpg_game":{"doc":"RPG Game","t":[],"n":[],"q":[],"d":[],"i":[],"f":[],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["rpg_game"] = {"name":"","dirs":[{"name":"test","files":["mod.rs","version_number.rs"]}],"files":["lib.rs"]};
+createSourceSidebar();
